4a110ca1506eb341532182d5b42928d898699feb
jyrex <jeffer.maliangkay7b@gmail.com>
2017-04-01 11:10:36 +0700

    On branch deploy
    added migration using routes
    adding down page

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -43,3 +43,33 @@ Route::group(['middleware' => 'auth'], function() {
 
 // AJAX
 Route::get('/fakultas/{fak_id}', 'Ajax\GetProdiController@get_prodi');
+
+Route::get('/install/{key?}',  array('as' => 'install', function($key = null)
+{
+    if($key == "appSetup_key"){
+    try {
+      echo '<br>init migrate:install...';
+      Artisan::call('migrate:install');
+      echo 'done migrate:install';
+
+      echo '<br>init with Sentry tables migrations...';
+      Artisan::call('migrate', [
+        '--package'=>'cartalyst/sentry'
+        ]);
+      echo 'done with Sentry';
+      echo '<br>init with app tables migrations...';
+      Artisan::call('migrate', [
+        '--path'     => "app/database/migrations"
+        ]);
+      echo '<br>done with app tables migrations';
+      echo '<br>init with Sentry tables seader...';
+      Artisan::call('db:seed');
+      echo '<br>done with Sentry tables seader';
+    } catch (Exception $e) {
+      Response::make($e->getMessage(), 500);
+    }
+  }else{
+    App::abort(404);
+  }
+}
+));
